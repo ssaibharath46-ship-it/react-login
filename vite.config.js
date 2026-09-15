1b0a7cf32d85e11daf39e060d14f77f96443f7bc
ssaibharath46-ship-it <ssaibharath46@gmail.com>
2026-09-15 10:07:04 +0530

    fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-login/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
